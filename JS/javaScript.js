b8e03714b75f5ec6414b22e79bcdc64c18f0c8df
joaosilva-web <joaogustavosilva2001@gmail.com>
2021-05-16 17:23:57 -0300

    sitePessoal

---
+++ JS/javaScript.js
@@ -0,0 +1,73 @@
+/*====scrolling animations====*/
+const debounce = function(func, wait, immediate) {
+    let timeout;
+    return function(...args) {
+        const context = this;
+        const later = function () {
+            timout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        const callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
+
+
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+function animeScroll(){
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 5);
+    target.forEach(function(element) {
+        if((windowTop) > element.offsetTop) {
+            element.classList.add(animationClass);
+        } else {
+            element.classList.remove(animationClass);
+        }
+    })
+}
+
+animeScroll();
+
+if(target.length) {
+window.addEventListener('scroll', debounce(function() {
+    animeScroll();
+}, 100));
+}
+
+
+
+
+
+
+
+/*====slider====*/
+let time = 5000,
+    currentImageIndex = 0,
+    images = document.querySelectorAll("#slider span")
+    max = images.length;
+
+function nextImage() {
+
+    images[currentImageIndex]
+        .classList.remove("selected")
+
+    currentImageIndex++
+
+    if(currentImageIndex >= max)
+        currentImageIndex = 0
+
+    images[currentImageIndex]
+        .classList.add("selected")
+}
+
+function start() {
+    setInterval(() => {
+        // troca de image
+        nextImage()
+    }, time)
+}
+
+window.addEventListener("load", start)
