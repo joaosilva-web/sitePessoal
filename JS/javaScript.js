379e2b7758677ddf629f033dde5fca5abc6bfe09
joaosilva-web <joaogustavosilva2001@gmail.com>
2021-05-17 18:43:01 -0300

    last-

--- JS/javaScript.js
+++ JS/javaScript.js
@@ -36,38 +36,3 @@ window.addEventListener('scroll', debounce(function() {
     animeScroll();
 }, 100));
 }
-
-
-
-
-
-
-
-/*====slider====*/
-let time = 6000,
-    currentImageIndex = 0,
-    images = document.querySelectorAll("#slider span")
-    max = images.length;
-
-function nextImage() {
-
-    images[currentImageIndex]
-        .classList.remove("selected")
-
-    currentImageIndex++
-
-    if(currentImageIndex >= max)
-        currentImageIndex = 0
-
-    images[currentImageIndex]
-        .classList.add("selected")
-}
-
-function start() {
-    setInterval(() => {
-        // troca de image
-        nextImage()
-    }, time)
-}
-
-window.addEventListener("load", start)
